b56d2e918b1ee5d928e532e935dafed73eb80d0e
Rafael Mohlin <raffe_mohlin@hotmail.com>
2018-12-04 23:51:45 +0100

    Breakout branch name and odb data

--- src/zsh-prompt.c
+++ src/zsh-prompt.c
@@ -8,6 +8,58 @@ void die_giterr(int error) {
     exit(error);
 }
 
+int get_head(git_reference **out_ref, git_repository *repo) {
+    int error;
+    git_reference *head_ref = NULL;
+    error = git_repository_head(&head_ref, repo);
+    if (error)
+        return error;
+    error = git_reference_resolve(out_ref, head_ref);
+    git_reference_free(head_ref);
+    return error;
+}
+
+int get_branch_name(const char **out_name, git_repository *repo, const git_oid *oid) {
+    int error;
+    git_branch_iterator *branches = NULL;
+    error = git_branch_iterator_new(&branches, repo, GIT_BRANCH_LOCAL);
+    if (error)
+        return error;
+
+    git_reference *branch_ref = NULL;
+    git_branch_t branch_type;
+    const git_oid *branch_oid = NULL;
+    while (!(error = git_branch_next(&branch_ref, &branch_type, branches))) {
+        branch_oid = git_reference_target(branch_ref);
+        if (git_oid_cmp(oid, branch_oid))
+            continue;
+
+        error = git_branch_name(out_name, branch_ref);
+        break;
+    }
+
+    git_branch_iterator_free(branches);
+    return error;
+}
+
+int get_odb_data(const void **out_data, size_t *out_size, git_odb *odb, const git_oid *oid) {
+    if (!git_odb_exists(odb, oid))
+        return GIT_ENOTFOUND;
+
+    int error;
+    git_odb_object *odb_object = NULL;
+    error = git_odb_read(&odb_object, odb, oid);
+    if (error)
+        return error;
+
+    if (out_size != NULL)
+        *out_size = git_odb_object_size(odb_object);
+    *out_data = git_odb_object_data(odb_object);
+
+    git_odb_object_free(odb_object);
+    return error;
+}
+
 int main() {
     git_libgit2_init();
     printf("Hello world! %s v%d.%d\n", PROJECT_NAME, PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR);
@@ -23,50 +75,22 @@ int main() {
             die_giterr(error);
 
         {
-            git_reference *head_refdirect = NULL;
-            {
-                git_reference *head_ref = NULL;
-                error = git_repository_head(&head_ref, repo);
-                if (error)
-                    die_giterr(error);
-                error = git_reference_resolve(&head_refdirect, head_ref);
-                if (error)
-                    die_giterr(error);
-                git_reference_free(head_ref);
-            }
+            git_reference *head_ref = NULL;
+            error = get_head(&head_ref, repo);
+            if (error)
+                die_giterr(error);
 
             const git_oid *head_oid = NULL;
-            head_oid = git_reference_target(head_refdirect);
+            head_oid = git_reference_target(head_ref);
             char head_shortsha[9] = {0};
             git_oid_tostr(head_shortsha, 8, head_oid);
             printf("Commit %s\n", head_shortsha);
 
-            {
-                git_branch_iterator *branches = NULL;
-                error = git_branch_iterator_new(&branches, repo, GIT_BRANCH_LOCAL);
-                if (error)
-                    die_giterr(error);
-
-                git_reference *branch_ref = NULL;
-                git_reference *branch_refdirect = NULL;
-                git_branch_t branch_type;
-                const git_oid *branch_oid = NULL;
-                const char *branch_name = NULL;
-                while (!(error = git_branch_next(&branch_ref, &branch_type, branches))) {
-                    error = git_branch_name(&branch_name, branch_ref);
-                    if (error)
-                        die_giterr(error);
-                    printf("Branch %s\n", branch_name);
-                    branch_oid = git_reference_target(branch_ref);
-                    if (!git_oid_cmp(head_oid, branch_oid)) {
-                        printf("^^^^ Current branch!\n");
-                    }
-                }
-                if (error != GIT_ITEROVER)
-                    die_giterr(error);
-
-                git_branch_iterator_free(branches);
-            }
+            const char *branch_name = NULL;
+            error = get_branch_name(&branch_name, repo, head_oid);
+            if (error)
+                die_giterr(error);
+            printf("Branch %s\n", branch_name);
 
             {
                 git_odb *odb = NULL;
@@ -74,24 +98,15 @@ int main() {
                 if (error)
                     die_giterr(error);
 
-                if (!git_odb_exists(odb, head_oid))
-                    printf("Detached\n");
-                else {
-                    git_odb_object *odb_object = NULL;
-                    error = git_odb_read(&odb_object, odb, head_oid);
-                    if (error)
-                        die_giterr(error);
-
-                    size_t data_size = git_odb_object_size(odb_object);
-                    const void *data = git_odb_object_data(odb_object);
-                    printf("====ODB data====\n%.*s================\n", data_size, data);
-
-                    git_odb_object_free(odb_object);
-                }
+                const void *data = NULL;
+                error = get_odb_data(&data, NULL, odb, head_oid);
+                if (error)
+                    die_giterr(error);
+                printf("====ODB data====\n%s================\n", data);
 
                 git_odb_free(odb);
             }
-            git_reference_free(head_refdirect);
+            git_reference_free(head_ref);
         }
         git_repository_free(repo);
     }
